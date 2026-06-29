059174e093589f7969e76b00b7df30e17c35c29b
chewst <234235836+chewst@users.noreply.github.com>
2026-06-29 16:19:37 +0800

    Add getters and grid copy method

---
+++ src/sudoku/problemdomain/SudokuGame.java
@@ -0,0 +1,26 @@
+package sudoku.problemdomain;
+
+public class SudokuGame {
+
+    private final GameState gameState;
+    private final int[][] gridState;
+
+    public static final int GRID_BOUNDARY = 9;
+
+    public SudokuGame(GameState gameState, int[][] gridState) {
+        this.gameState = gameState;
+        this.gridState = gridState;
+    }
+
+    public int[][] getGridState() {
+        return gridState;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public int[][] getCopyOfGridState() {
+        return SudokuUtilities.copyToNewArray(gridState);
+    }
+}
