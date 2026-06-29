da699fce8c47e664860b7559a24422ae4b2774ac
chewst <234235836+chewst@users.noreply.github.com>
2026-06-29 17:14:42 +0800

    Add Javadoc for Storage Interface for clarity

--- src/sudoku/problemdomain/IStorage.java
+++ src/sudoku/problemdomain/IStorage.java
@@ -2,7 +2,33 @@ package sudoku.problemdomain;
 
 import java.io.IOException;
 
+/**
+ * Defines a contract for storing and retrieving Sudoku game data.
+ *
+ * Implementations of this interface are responsible for handling
+ * the persistence of a SudokuGame, such as saving to a file,
+ * database, or other storage medium.
+ *
+ * Note:
+ * - Methods may throw IOException if a read/write operation fails.
+ * - This abstraction allows the storage mechanism to be changed
+ *   without affecting the core game logic.
+ */
 public interface IStorage {
+
+    /**
+     * Saves or updates the current Sudoku game data.
+     *
+     * @param game the SudokuGame instance to be stored
+     * @throws IOException if an error occurs during writing
+     */
     void updateGameData(SudokuGame game) throws IOException;
+
+    /**
+     * Retrieves the stored Sudoku game data.
+     *
+     * @return the previously saved SudokuGame
+     * @throws IOException if an error occurs during reading
+     */
     SudokuGame getGameData() throws IOException;
 }
