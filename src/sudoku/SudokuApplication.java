6cf398572ffed8cc4b2c05e929d8937b1471e032
chewst <234235836+chewst@users.noreply.github.com>
2026-06-30 23:36:02 +0800

    Refactor application startup to use JavaFX - add SudokuApplication as JavaFX entry point - initialize UI through UserInterfaceImpl - delegate setup to SudokuBuildLogic

---
+++ src/sudoku/SudokuApplication.java
@@ -0,0 +1,37 @@
+package sudoku;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class SudokuApplication extends Application{
+
+    private IUserInterfaceContract.View uiImpl;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        uiImpl = new UserInterfaceImpl(primaryStage);
+        try {
+            SudokuBuildLogic.build(uiImpl);
+        } catch (IOException e){
+            e.printStackTrace();
+            throw e;
+        }
+
+//        Parent root = FXMLLoader.load(getClass().getResource("sample");
+//        primaryStage.setTiltle("Hello World");
+//        primaryStage.setScene(new Scene(root, 300, 275));
+//        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
