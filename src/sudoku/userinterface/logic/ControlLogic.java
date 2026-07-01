f13945296dd5b4b9f353f5c781d57f3f998d0dcf
chewst <234235836+chewst@users.noreply.github.com>
2026-07-01 20:18:59 +0800

    implement control logic for handling Sudoku input and game updates

---
+++ src/sudoku/userinterface/logic/ControlLogic.java
@@ -0,0 +1,57 @@
+package sudoku.userinterface.logic;
+
+import sudoku.constants.GameState;
+import sudoku.constants.Messages;
+import sudoku.problemdomain.IStorage;
+import sudoku.problemdomain.SudokuGame;
+import sudoku.userinterface.IUserInterfaceContract;
+
+import java.io.IOException;
+
+public class ControlLogic implements IUserInterfaceContract.EventListener {
+
+    private IStorage storage;
+
+    private IUserInterfaceContract.View view;
+
+    public ControlLogic(IStorage storage, IUserInterfaceContract.View view) {
+        this.storage = storage;
+        this.view = view;
+    }
+
+    @Override
+    public void onSudokuInput(int x, int y, int input) {
+        try{
+            SudokuGame gameData = storage.getGameData();
+
+            // work on a copy to avoid mutating original directly
+            int[][] newGridState = gameData.getCopyOfGridState();
+
+            newGridState[x][y] = input; // apply user input to selected cell
+
+            // create new game instance with updated state
+            gameData = new SudokuGame(
+                    GameLogic.checkForCompletion(newGridState),
+                    newGridState
+            );
+
+            storage.updateGameData(gameData);  // persist updated game state
+
+            view.updateSquare(x ,y ,input); // update UI for the changed cell only
+
+            // if game is complete, pop up the CONFIRMATION dialog
+            if(gameData.getGameState() == GameState.COMPLETE){
+                view.showDialog(Messages.GAME_COMPLETE);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            view.showError(Messages.ERROR);
+        }
+    }
+
+    @Override
+    public void onDialogClick() {
+
+    }
+}
